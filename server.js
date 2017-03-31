10c767215ba94b94bc8c47e0064785bafb7f3648
Dilroop singh <dilroop.singh.ds@gmail.com>
2017-03-31 15:45:12 +0530

    server js changes

--- server.js
+++ server.js
@@ -50,15 +50,188 @@ global.__logger = new (winston.Logger)({
 */
 var ajax = require("./ajax");
 
+var constant=require("./CONSTANTS");
+
 // Open API for receieving POst req
+
+		
+		
+		
+		
+
 app.get('/sendSMS', function(req, res){
-
-console.log("asd")
-   ajax.sendSMS("Hello","918968999927");
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
+ 
+console.log("asd");
+   //ajax.sendSMS("positive case ","");
+   
+var username = "saurabh";
+    var pass = "Dhis1234";
+	var auth = "Basic " + new Buffer(username + ":" + pass).toString("base64");
+	console.log(auth);
+	
+	ajax.getReq("http://localhost:8080/aes/api/events.json?program=a9cQSlDVI2n&orgUnit=zpkwWCuP8oc&trackedEntityInstance=ensvQUMh8FF&paging=false",auth,callback);
+	
+  res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('ok');
+	
+	function callback(error,response,body){
+		//console.log(error);
+		
+		var data=JSON.parse(body);
+		//console.log(data);
+		
+		for(var i=0;i<data.events.length;i++)
+		{
+			
+				for(var k=0;k<data.events[i].dataValues.length;k++)
+				{
+			var id=data.events[i].dataValues[k].dataElement;
+			
+		
+			if(id==constant.sample_sent_to_Apex_Lab)              //  Is CSF sample sent to Apex Lab?
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="true")
+				{
+					console.log("       "+val);
+				}
+			}
+			//-----------------------
+			
+			else if(id==constant.CSF_sample_2_JE_IgM)              //  Lab results - CSF sample 2 - JE IgM
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//-----------------------
+			
+			else if(id==constant.Lab_results_CSF_JE_IgM)              //  Lab results - CSF - JE IgM
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//-----------------------
+			
+			else if(id==constant.Lab_results_Malaria_rapid_diagnostic_test)              //  Lab results - Malaria rapid diagnostic test"
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="V +")
+				{
+					console.log("       "+val);
+				}
+			}
+			//----------------------
+		
+			else if(id==constant.Lab_results_Serum_sample_2_JE_gM)              //  Lab results - Serum sample 2 - JE IgM"
+			{// if (id == exports.labResultSerumSample2_JeIgm)
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//----------------------
+			
+			else if(id==constant.Lab_results_Serum_JE_IgM)              //  Lab results - Serum - JE IgM"
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//----------------------
+			
+			else if(id==constant.Lab_results_Malaria_smear_results)              //  Lab results -Malaria smear results
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="PV")
+				{
+					console.log("       "+val);
+				}
+			}
+			//--------------------
+		
+			
+			//---------------------
+		
+			else if(id==constant.Follow_up_visit_Lab_results_Serum_JE_IgM)              // Follow up visit - Lab results - Serum - JE IgM
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//----------------------
+		
+			else if(id==constant.Follow_up_visit_Lab_results_Serum_Scrub_typhus_IgG)              // Follow up visit - Lab results - Serum - JE IgM
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//--------------------
+			
+			else if(id==constant.Lab_results_Serum_IgM_DEN)              // Lab results - Serum - IgM DEN
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			//----------------------
+			
+			 else if(id==constant.Scrub_typhus_IgM)              // Lab results - Serum - Scrub typhus IgM
+			{
+				console.log(id);
+				var val=data.events[i].dataValues[k].value;
+				if(val=="Positive")
+				{
+					console.log("       "+val);
+				}
+			}
+			
+			//-----------------------
+				}
+		}
+		
+		// K24hMmaJvrV   Lab results - CSF sample 2 - JE IgM"
+		//N6Yfs0jO6FY    Lab results - CSF - JE IgM
+		//v5WLB0ggt1y   Lab results - Malaria rapid diagnostic test"
+		//KVJSXRivsHL   Lab results - Serum sample 2 - JE IgM"
+		// DG9PTsQmliZ  Lab results - Serum - JE IgM
+		// kspIAuMSEXO Lab results - Malaria smear results"
+		// v5WLB0ggt1y "Lab results - Malaria rapid diagnostic test"
+		// LpT8hxDYDHq  "Follow up visit - Lab results - Serum - JE IgM"
+		// SyVZXV49iO9  "Follow up visit - Lab results - Serum - Scrub typhus IgG
+		// DHpb0qQ61ZE Lab results - Serum - IgM DEN
+		// LhrbwYEYv20 Lab results - Serum - Scrub typhus IgM
+	}
+	
 });
+				
+				
+
 
 var server = app.listen(8000, function () {
 
@@ -66,6 +239,7 @@ var server = app.listen(8000, function () {
     var port = server.address().port
 
     console.log("Server listening at http://%s:%s", host, port)
+
 
 })
 
